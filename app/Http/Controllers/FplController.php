48f1a004b37150d0a01a72b96ab976a4abd545a0
Ayesha <ayeshaakhter222@gmail.com>
2021-10-05 12:03:47 +0600

    first commit

---
+++ app/Http/Controllers/FplController.php
@@ -0,0 +1,31 @@
+<?php
+
+// namespace App\Http\Controllers;
+// use Illuminate\Support\Facades\Http;
+
+// use Illuminate\Http\Request;
+
+// class FplController extends Controller
+// {
+//      public function getdata(){
+//         $response = Http::get('https://fantasy.premierleague.com/api/bootstrap-static/');
+//         $response = $response->json()['elements'];
+//         return $response;
+//     }
+// }
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class FplController extends Controller
+{
+    public function getdata(){
+        $response = Http::get('https://fantasy.premierleague.com/api/bootstrap-static/');
+        $response = $response->json()['elements'];
+        return $response;
+    }
+}
+
